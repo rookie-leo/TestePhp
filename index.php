e1235c2994119739a797eed752f31474fa5d7b0f
Leonardo Paulino <60435208+rookie-leo@users.noreply.github.com>
2022-03-09 00:40:39 +0000

    Aula 08_03

--- index.php
+++ index.php
@@ -2,7 +2,25 @@
   <head>
     <title>PHP Test</title>
   </head>
+  
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+  <h1> Texto em HTML! </h1>
+    
+    <?php
+      echo '<h1>Texto em PHP!</h1>';  
+      $dia = 8;
+      $mes = 3;
+      $ano = 2022;
+
+      echo 'Hoje é dia '.$dia.'/'.$mes.'/'.$ano;
+
+      $data_atual .= $dia;
+      $data_atual .= "/".$mes;
+      $data_atual .= "/".$ano;
+      $data_atual .= "<br>";
+
+      echo "<br>Exemplo de concatenação ".$data_atual;
+      echo "Soma = ".($dia + $mes);
+    ?> 
   </body>
 </html>
